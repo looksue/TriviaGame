843eafcbe0bb1e8ce13db5a7e8fc1929949b37ce
Sue Look <suelook1@gmail.com>
2019-08-11 15:45:10 -0400

    update the event listener within function

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,17 +1,30 @@
-
 let timeRemaining = document.getElementById("spanTimeRemaining");
 let choices = document.getElementById("divChoices");
 let start = document.getElementById("divStart");
 let trivia = document.getElementById("divTrivia");
-let wrongGuess = document.getElementById("divWrongGuess");
+let guess = document.getElementById("divGuess");
+let image = document.getElementById("divImage");
 let score = document.getElementById("divScore");
 let question = document.getElementById("pQuestion");
 let btnStart = document.getElementById("btnStart");
 
-var choiceA=document.getElementById("choiceA");
-var choiceB=document.getElementById("choiceB");
-var choiceC=document.getElementById("choiceC");
-var choiceD=document.getElementById("choiceD");
+var choiceA = document.getElementById("choiceA");
+var choiceB = document.getElementById("choiceB");
+var choiceC = document.getElementById("choiceC");
+var choiceD = document.getElementById("choiceD");
+
+var correctGuesses = 0;
+var wrongGuesses = 0;
+var unansweredQuestions = 0;
+
+//delay timer
+var delay = ( function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
 
 //start the game by pressing start button
 
@@ -21,32 +34,75 @@ btnStart.addEventListener("click", function() {
     runGame();
 });
 
-function runGame() {
-    // put 120 seconds on the clock
-    var foo = setTimeout(endGame(), 1000 * 120);
-    var secondsLeft = 120;
-    
+function updateTimer(){
+    // var foo = setTimeout(endGame(), 1000 * 120);
+    var secondsLeft = 10;
     // update time remaining
     var bar = setInterval(function() {
         if (secondsLeft > 0) {
             secondsLeft--;
             timeRemaining.innerHTML = secondsLeft;
-
-            // display the first question, listen for clicks
-            for (var i = 0; i < questionArray.length - 1; i++){
-                pQuestion.innerHTML = questionArray[i]["question"];
-            }
-
-            // evaluate user choice, update display
-
-            // advance to next question
-
-            // display final result
-
         } else {
             endGame();
         }
     }, 1000);
+}
+
+function runGame() {
+
+    // display the questions, listen for clicks
+    for (var i = 0; i < questionArray.length -1; i++){
+        pQuestion.innerHTML = questionArray[i]["question"];
+        choiceA.innerHTML = questionArray[i]["choiceA"];
+        choiceB.innerHTML = questionArray[i]["choiceB"];
+        choiceC.innerHTML = questionArray[i]["choiceC"];
+        choiceD.innerHTML = questionArray[i]["choiceD"];
+        guess.innerHTML = "";
+        image.innerHTML = "";
+
+        console.log("Correct answer: " + questionArray[i]["correct"]);
+
+        // listen for user choices
+        choiceA.addEventListener("click", function() {
+            if (questionArray[i]["correct"] === "A") {
+                //display thats correct 
+                guess.innerHTML = "That's the correct answer!";
+                //display the picture
+                image.innerHTML = "<img src='" + questionArray[i]["imgSrc"] + "'>";
+                //add 1 to correct guesses
+                correctGuesses = correctGuesses +1;
+                //wait 3 seconds 
+                delay(function(){
+                    // do stuff
+                }, 3000 ); // end delay
+                //next questions
+            } else {
+                //display wrong guess
+                guess.innerHTML = "That's the wrong answer!  The correct answer was " + questionArray[i]["correctanswer"];
+                //diplay the picture
+                image.innerHTML = "<img src='" + questionArray[i]["imgSrc"] + "'>";
+                //add 1 to wrong guesses
+                wrongGuesses = wrongGuesses + 1;
+                //wait 3 seconds
+                delay(function(){
+                    // do stuff
+                }, 3000 ); // end delay
+                //next questions
+            }
+        });
+        choiceB.addEventListener("click", function() {
+
+        });
+        choiceC.addEventListener("click", function() {
+
+        });
+        choiceD.addEventListener("click", function() {
+
+        });
+
+    // display final result
+
+    }
 }
 
 function endGame() {
@@ -62,6 +118,7 @@ let questionArray = [
         choiceC: "Andrew Jackson",
         choiceD: "George Washington",
         correct: "D",
+        correctanswer: "George Washington",
         imgSrc: "assets/images/george.jpg"
     },
     {
@@ -71,6 +128,7 @@ let questionArray = [
         choiceC: "Theodore Roosevelt",
         choiceD: "Woodrow Wilson",
         correct: "A",
+        correctanswer: "Jimmy Carter",
         imgSrc: "assets/images/jimmyreading.jpg"
     },
     {
@@ -80,6 +138,7 @@ let questionArray = [
         choiceC: "George W. Bush",
         choiceD: "Bill Clinton",
         correct: "B",
+        correctanswer: "Barack Obama",
         imgSrc: "assets/images/obamaspiderman.jpg"
     },
     {
@@ -89,6 +148,7 @@ let questionArray = [
         choiceC: "Harry Truman",
         choiceD: "Theodore Roosevelt",
         correct: "D",
+        correctanswer: "Theodore Roosevelt",
         imgSrc: "assets/images/thewhitehouse.jpg"
     },
     {
@@ -98,7 +158,7 @@ let questionArray = [
         choiceC: "Harry Truman",
         choiceD: "John F. Kennedy",
         correct: "C",
+        correctanswer: "Harry Truman",
         imgSrc: "assets/images/harrytv.jpg"
     } 
 ];
-
